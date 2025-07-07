482d0f79d85671b7d2a31727abe2dac1c1cb6bf9
aliyannaaa <cornelkyla97@gmail.com>
2025-07-07 14:20:44 +0800

    changed homepage, added app component profile, added scan profile

--- src/app/buddy/buddy.page.ts
+++ src/app/buddy/buddy.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../service/firebase.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-buddy',
@@ -7,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   standalone: false,
 })
 export class BuddyPage implements OnInit {
+  buddyFirstName = '';
+  buddyLastName = '';
+  buddies: any[] = [];
 
-  constructor() { }
+  constructor(
+    private firebaseService: FirebaseService,
+    private toastController: ToastController
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.loadBuddies();
   }
 
+  async loadBuddies() {
+    this.buddies = await this.firebaseService.getBuddies();
+  }
+
+  async addBuddy() {
+    const buddy = {
+      firstName: this.buddyFirstName,
+      lastName: this.buddyLastName
+    };
+
+    try {
+      await this.firebaseService.addBuddy(buddy);
+      this.buddyFirstName = '';
+      this.buddyLastName = '';
+
+      const toast = await this.toastController.create({
+        message: 'Buddy added successfully!',
+        duration: 2000,
+        color: 'success'
+      });
+      await toast.present();
+
+      this.loadBuddies(); // Refresh list
+
+    } catch (error) {
+      const toast = await this.toastController.create({
+        message: 'Failed to add buddy.',
+        duration: 2000,
+        color: 'danger'
+      });
+      await toast.present();
+    }
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FirebaseService } from '../service/firebase.service'; // make sure this path is correct
+import { FirebaseService } from '../service/firebase.service';
+import { ToastController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,11 @@ export class HomePage {
   buddyFirstName = '';
   buddyLastName = '';
 
-  constructor(private firebaseService: FirebaseService) {}
+  constructor(
+    private firebaseService: FirebaseService,
+    private toastController: ToastController,
+    private alertController: AlertController
+  ) {}
 
   async addBuddy() {
     const buddy = {
@@ -24,8 +29,59 @@ export class HomePage {
       console.log('Buddy added with ID:', id);
       this.buddyFirstName = '';
       this.buddyLastName = '';
+
+      const toast = await this.toastController.create({
+        message: 'Buddy added successfully!',
+        duration: 2000,
+        color: 'success'
+      });
+      await toast.present();
+
     } catch (error) {
       console.error('Error adding buddy:', error);
+      const toast = await this.toastController.create({
+        message: 'Failed to add buddy.',
+        duration: 2000,
+        color: 'danger'
+      });
+      await toast.present();
     }
+  }
+
+  triggerEmergency() {
+    this.presentEmergencyConfirmation();
+  }
+
+  async presentEmergencyConfirmation() {
+    const alert = await this.alertController.create({
+      header: 'EMERGENCY ALERT!',
+      message: 'Your emergency alert is about to be sent. Are you sure?',
+      buttons: [
+        {
+          text: 'SEND ALERT',
+          handler: () => {
+            this.sendEmergencyAlert();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async sendEmergencyAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert Sent!',
+      message: `Your emergency alert has been sent to:<br><strong>The Emergency Services<br>Buddies</strong><br><br>Your location is being shared in real-time.`,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    // TODO: Add your actual emergency logic (Firebase, SMS, location, etc.)
   }
 }
