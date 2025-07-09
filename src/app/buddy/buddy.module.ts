737864e8b3da549bc23883076ff76cd23a5a365a
PatDep-1 <patricia.deiparine.22@usjr.edu.ph>
2025-07-09 12:43:58 +0800

    changed buddy page and some scss of profile, scan and home

--- src/app/buddy/buddy.module.ts
+++ src/app/buddy/buddy.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { BuddyPageRoutingModule } from './buddy-routing.module';
 
 import { BuddyPage } from './buddy.page';
+import { BuddyModalComponent } from './buddy-modal.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { BuddyPage } from './buddy.page';
     IonicModule,
     BuddyPageRoutingModule
   ],
-  declarations: [BuddyPage]
+  declarations: [BuddyPage, BuddyModalComponent]
 })
 export class BuddyPageModule {}
